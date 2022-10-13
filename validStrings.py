a489267d31303f71a12fd0f19b89e5292de79b82
TheWillni <108456304+TheWillni@users.noreply.github.com>
2022-10-13 19:35:48 +1100

    Tests out the wazoo
    - creates both valid and invalid tests
    - creates n amounts of tests

--- validStrings.py
+++ validStrings.py
@@ -12,19 +12,67 @@ import random
 
 MIN_N = 0
 MIN_M = 1
-MAX_N = 20
-MAX_M = 20
-MAX_R = 50
+MAX_N = 3
+MAX_M = 3
+MAX_R = 20
+MAX_VAL = 20
+MAX_TESTS = 20
+execute = "runDPDA dpdaR "
+wantValid = True
 
-n = random.randint(MIN_N, MAX_N)
-m = random.randint(MIN_M, MAX_M)
+def getRandomStringValid(i):
+    n = getIntInRange(MIN_N, MAX_N, True)
+    m = getIntInRange(MIN_M, MAX_M, True)
 
-# added plus to make sure r is strictly greater than
-r = random.randint( ((2*n) + m + 2), MAX_R)
+    # added plus to make sure r is strictly greater than
+    r = getIntInRange(((2*n) + m + 2), MAX_R, True)
+    
+    
+    valid_string = "a"*n + "b"*r + "c"*m
+    
+    printResult(valid_string, n, m, r)
+    return ""
 
-valid_string = "a"*n + "b"*r + "c"*m
+def getRandomStringInvalid(i):
+    n = getIntInRange(MIN_N, MAX_N, False)
+    m = getIntInRange(MIN_M, MAX_M, False)
 
+    # added plus to make sure r is strictly greater than
+    r = getIntInRange(((2*n) + m + 2), MAX_R, False)
 
-print("Generated values of (n,m,r) = (", n, ",", m, ",", r, ")")
-print("Valid string --->", valid_string)
+    valid_string = "a"*n + "b"*r + "c"*m
+    printResult(valid_string, n, m, r)
+    return ""
+
+def printResult(string, m, n ,r):
+    print("Test case where R is [", isValidR(n,m,r), "]: #", i)
+    print("Generated values of (n,m,r) = (", n, ",", m, ",", r, ")")
+    print ("see proof: ", (r), ">", "2*",n,"+", m,"+ 1" + "    [", r, ">", ((2*n) +m+1), "]")
+    print("Valid string:")
+    print(string)
+    print(execute +"\"" + string +"\"" + "\n")
+    
+def isValidR(n, m, r): 
+    if ( r > ( (2*n) + m + 1)):
+        return True
+    else:
+        return False
+
+def getIntInRange(minValue, maxValue, wantInRange):
+    notAcceptable = True
+    while (notAcceptable):
+        a = random.randint(0,MAX_VAL)
+        if (wantInRange):
+            if (a >= minValue and a <= maxValue):
+                return a
+        else:
+            if (not (a >= minValue and a <= maxValue)):
+                return a
+        
+for i in (range(1,MAX_TESTS+1)):
+    getRandomStringValid(i)
+
+for i in (range(1,MAX_TESTS+1)):
+    getRandomStringInvalid(i)
+
 
